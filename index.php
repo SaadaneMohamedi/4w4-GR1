7b0c6609535a860dd08cbea7431bb023e7118fc2
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-02-02 11:51:26 -0500

    création de front-page.php et single.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
                 <article>
                     <h3><?php get_the_title();?></h3>
                     <h6>Extrait:</h6> <?php the_excerpt();?>
-                    <h6>Contenu:</h6><?php the_content();?></h6>
+                    <h6>Contenu:</h6><?php the_content();?>
                 </article>
             <?php endwhile;
         endif;
--- single.php
+++ single.php
@@ -1,12 +1,12 @@
 <?php get_header()?>
-    <pre>front-page.php</pre>
+    <pre>single.php</pre>
     <main><h1>Bienvenue sur 4W4</h1></main>
     <?php
         if (have_posts()):
             while (have_posts()): the_post();?>
                 <article>
-                    <a href="<?php the_permalink();?>"><h3><?php the_title();?></h3></a>
-                    <?php the_excerpt();?>
+                    <h3><?php the_title();?></h3>
+                    <?php the_content();?>
                 </article>
                 <hr>
             <?php endwhile;?>
