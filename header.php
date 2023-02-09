ca0491ea5168601239e9d9d06725925e703f0acd
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-02-09 11:49:23 -0500

    exr1 partie 2: styles flex appliqués pour le logo et le menu

--- header.php
+++ header.php
@@ -9,8 +9,10 @@
 </head>
 <body>
     <header class="site__entete">
-        <?php the_custom_logo()?>
-        <?php wp_nav_menu(array("menu" => "entete", "container" => "nav", "container_class" => "menu__entete"));?>
+        <section class="site__header__logo">
+            <?php the_custom_logo()?>
+            <?php wp_nav_menu(array("menu" => "entete", "container" => "nav", "container_class" => "menu__entete"));?>
+        </section>
         <h1><a class="site__titre" href="<?= bloginfo('url');?>"><?= bloginfo('name');?></a></h1>
         <h2><?= bloginfo('description');?></h2>
         <h3 class="h3"><?= bloginfo('url');?></h3>
