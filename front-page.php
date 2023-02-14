66d3585b7a5b401bc12da8dfffa3a789f03c6d77
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-02-14 11:24:51 -0500

    trim_word et corrections blocflex

--- front-page.php
+++ front-page.php
@@ -1,13 +1,14 @@
 <?php get_header()?>
     <pre>front-page.php</pre>
     <main><h1>Bienvenue sur 4W4</h1></main>
-    <section class="blocflexx">
+    <section class="blocflex">
     <?php
         if (have_posts()):
             while (have_posts()): the_post();?>
                 <article>
                     <a href="<?php the_permalink();?>"><h3><?php the_title();?></h3></a>
                     <?php the_excerpt();?>
+                    <?php wp_trim_words(get_the_excerpt(), 10, "...")?>
                 </article>
             <?php endwhile;?>
         <?php endif;?>
