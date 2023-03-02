90af24b3f4d87781eb64591a8515998111710e8b
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-03-02 11:13:17 -0500

    intégration de la fonction template-parts dans le thème

--- category.php
+++ category.php
@@ -16,20 +16,11 @@
         </section> -->
         <section class="blocflex">
             <?php $category = get_queried_object(); $args = array( 'category_name' => $category->slug, 'orderby' => 'title', 'order' => 'ASC');
-            $query = new WP_Query( $args );
-            if ( $query->have_posts() ) :
-                while ( $query->have_posts() ) : $query->the_post();
-                $titre = get_the_title();
-                if ($query->slug == "cours") {
-                    $sigle = substr($titre, 0, 7);
-                    $titre_long = substr($titre, 7, -5);
-                    $duree = "90";
-                    $titre = $titre_long;
-                }?>
-                    <article>
-                        <a href="<?php the_permalink(); ?>"><h3><?= $titre;?></h3></a>
-                        <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
-                    </article>
+            $query = new WP_Query($args);
+            if ($query->have_posts() ) :
+                while ($query->have_posts()) : $query->the_post();
+                get_template_part('template-parts/categorie', $category->slug);
+                ?>
                 <?php endwhile; ?>
             <?php endif; wp_reset_postdata();?>
         </section>
---
+++ template-parts/categorie-4w4.php
@@ -0,0 +1,10 @@
+<?php 
+    $titre = get_the_title();
+    /*if (substr(0,1) == "0") {
+        
+    }*/
+?>
+<article>
+    <h5><a href="<?php the_permalink(); ?>"> <?= $titre; ?></a></h5>
+    <p><?= wp_trim_words(get_the_excerpt(), 15)?></p>
+</article>
