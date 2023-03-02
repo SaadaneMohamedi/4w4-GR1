6e5bf6761a51d2cdc6db03eb53a6ff451b3d1596
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-03-02 09:46:50 -0500

    normalisation et ajustement des styles appliqués aux menus et aux articles

--- header.php
+++ header.php
@@ -12,8 +12,9 @@
         <section class="site__header__logo">
             <?php the_custom_logo()?>
             <?php wp_nav_menu(array("menu" => "entete", "container" => "nav", "container_class" => "menu__entete"));?>
-            <?php get_search_form()?>
         </section>
+        <br>
+        <?php get_search_form()?>
         <h1><a class="site__titre" href="<?= bloginfo('url');?>"><?= bloginfo('name');?></a></h1>
         <h2><?= bloginfo('description');?></h2>
         <h3 class="h3"><?= bloginfo('url');?></h3>
