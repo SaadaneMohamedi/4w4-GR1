6fe93811318981463e15be59bf744ed2decb870e
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-03-09 11:45:57 -0500

    normalisation de la barre de recherche et adaptabilité mobile/desktop

--- header.php
+++ header.php
@@ -11,10 +11,11 @@
     <header class="site__entete">
         <section class="site__header__logo">
             <?php the_custom_logo()?>
-            <?php wp_nav_menu(array("menu" => "entete", "container" => "nav", "container_class" => "menu__entete"));?>
+            <div class="menu__recherche">
+                <?php wp_nav_menu(array("menu" => "entete", "container" => "nav", "container_class" => "menu__entete"));?>
+                <?php get_search_form()?>
+            </div>
         </section>
-        <br>
-        <?php get_search_form()?>
         <h1><a class="site__titre" href="<?= bloginfo('url');?>"><?= bloginfo('name');?></a></h1>
         <h2><?= bloginfo('description');?></h2>
     </header>
