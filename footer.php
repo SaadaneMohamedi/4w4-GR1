51d1e03781afd493cb5b524bd0e7a4c1db2ed4b4
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-03-16 11:15:46 -0400

    correction apportée au footer

--- footer.php
+++ footer.php
@@ -1,12 +1,8 @@
 <footer class="site__footer">
-<section class="site_footer_logo">
  <?php the_custom_logo();?>
  <nav class="footer">
     <h5>Plus ici!</h5>
-    <?php wp_nav_menu(array(
-        "menu" => "footer",
-        "container" => "nav",
-        "container_class" => "menufooter", )); ?>
+    <?php wp_nav_menu(array("menu" => "footer", "container" => "nav", "container_class" => "menufooter", )); ?>
 </nav>
 </footer>
 <?php wp_footer(); ?>
