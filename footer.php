1e07da881effbda27bb2d90202426165e9c3ed75
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-03-16 11:16:41 -0400

    correction apportée au footer pt.2

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <footer class="site__footer">
- <?php the_custom_logo();?>
  <nav class="footer">
     <h5>Plus ici!</h5>
     <?php wp_nav_menu(array("menu" => "footer", "container" => "nav", "container_class" => "menufooter", )); ?>
