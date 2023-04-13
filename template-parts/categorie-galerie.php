7bf9a276e43bbce2982d69474cc087b1fa8c61e7
Saadane <e2075430@cmaisonneuve.qc.ca>
2023-04-13 13:30:36 -0400

    intégration d'un ACF de type Liste-Déroulante

--- template-parts/categorie-galerie.php
+++ template-parts/categorie-galerie.php
@@ -1,4 +1,5 @@
 <article class="blocflex__galerie">
     <?php the_content();?>
+    <p><?php the_field('liste-deroulate');?></p>
     <p>Source: <a href="facebook.com/maisonneuvetim/"><?php the_field('url-galerie');?></a></p>
 </article>
